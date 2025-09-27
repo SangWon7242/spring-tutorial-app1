049b2c87f0ccc1537da32e25afa45bb787680c9e
SangWon7242 <gotkdqja@gmail.com>
2025-09-27 10:49:25 +0900

    프로젝트 전체 세팅

---
+++ src/main/generated/com/sbs/tutorial/app1/domain/fileUpload/entity/QGenFile.java
@@ -0,0 +1,68 @@
+package com.sbs.tutorial.app1.domain.fileUpload.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QGenFile is a Querydsl query type for GenFile
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QGenFile extends EntityPathBase<GenFile> {
+
+    private static final long serialVersionUID = 1734963885L;
+
+    public static final QGenFile genFile = new QGenFile("genFile");
+
+    public final com.sbs.tutorial.base.entity.QBaseEntity _super = new com.sbs.tutorial.base.entity.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    public final StringPath fileDir = createString("fileDir");
+
+    public final StringPath fileExt = createString("fileExt");
+
+    public final StringPath fileExtType2Code = createString("fileExtType2Code");
+
+    public final StringPath fileExtTypeCode = createString("fileExtTypeCode");
+
+    public final NumberPath<Integer> fileNo = createNumber("fileNo", Integer.class);
+
+    public final NumberPath<Integer> fileSize = createNumber("fileSize", Integer.class);
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifyDate = _super.modifyDate;
+
+    public final StringPath originFileName = createString("originFileName");
+
+    public final NumberPath<Integer> relId = createNumber("relId", Integer.class);
+
+    public final StringPath relTypeCode = createString("relTypeCode");
+
+    public final StringPath type2Code = createString("type2Code");
+
+    public final StringPath typeCode = createString("typeCode");
+
+    public QGenFile(String variable) {
+        super(GenFile.class, forVariable(variable));
+    }
+
+    public QGenFile(Path<? extends GenFile> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QGenFile(PathMetadata metadata) {
+        super(GenFile.class, metadata);
+    }
+
+}
+
---
+++ src/main/java/com/sbs/tutorial/app1/domain/fileUpload/entity/GenFile.java
@@ -0,0 +1,28 @@
+package com.sbs.tutorial.app1.domain.fileUpload.entity;
+
+
+import com.sbs.tutorial.base.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class GenFile extends BaseEntity {
+  private String relTypeCode;
+  private int relId;
+  private String typeCode;
+  private String type2Code;
+  private String fileExtTypeCode;
+  private String fileExtType2Code;
+  private int fileSize;
+  private int fileNo;
+  private String fileExt;
+  private String fileDir;
+  private String originFileName;
+}
