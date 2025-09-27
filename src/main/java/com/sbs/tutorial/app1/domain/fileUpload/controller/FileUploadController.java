55f9ce4b1e164cb240b13699888e018875c56b95
SangWon7242 <gotkdqja@gmail.com>
2025-09-27 11:33:24 +0900

    step3 : 설정 파일에서 파일 저장 경로 가져오기

--- src/main/java/com/sbs/tutorial/app1/domain/fileUpload/controller/FileUploadController.java
+++ src/main/java/com/sbs/tutorial/app1/domain/fileUpload/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.sbs.tutorial.app1.domain.fileUpload.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +15,14 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @RequestMapping("/upload")
 public class FileUploadController {
+  @Value("${custom.genFileDirPath}")
+  private String genFileDirPath;
+
   @RequestMapping("")
   @ResponseBody
   public String upload(@RequestParam("img1") MultipartFile img1, @RequestParam("img2") MultipartFile img2) {
-    String uploadDir = "c:/spring-temp/app1"; // 저장 디렉토리
-    
     // 파일 저장 경로 생성
-    File directory = new File(uploadDir);
+    File directory = new File(genFileDirPath);
 
     if(!directory.exists()) {
       directory.mkdirs(); // 경로가 없으면 디렉토리 생성
