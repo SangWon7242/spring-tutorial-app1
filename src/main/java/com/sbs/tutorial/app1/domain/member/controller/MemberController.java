3b235b90655d5b7bade186583ffdddb268244f25
SangWon7242 <gotkdqja@gmail.com>
2025-09-28 10:35:58 +0900

    회원 가입 후 프로필 페이지로 리다이렉트

--- src/main/java/com/sbs/tutorial/app1/domain/member/controller/MemberController.java
+++ src/main/java/com/sbs/tutorial/app1/domain/member/controller/MemberController.java
@@ -3,8 +3,10 @@ package com.sbs.tutorial.app1.domain.member.controller;
 import com.sbs.tutorial.app1.domain.member.entity.Member;
 import com.sbs.tutorial.app1.domain.member.form.MemberJoinForm;
 import com.sbs.tutorial.app1.domain.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,18 +24,36 @@ public class MemberController {
   }
 
   @PostMapping("/join")
-  @ResponseBody
-  public Member join(MemberJoinForm memberJoinForm) {
+  public String join(MemberJoinForm memberJoinForm, HttpSession session) {
     String username = memberJoinForm.getUsername();
 
     Member oldMember = memberService.getMemberByUsername(username);
 
     if(oldMember != null) {
-      return null; // 이미 가입된 회원
+      return "redirect:/?errorMsg=Already exists username";
     }
     
     Member member = memberService.join(memberJoinForm);
 
-    return member;
+    session.setAttribute("loginedMemberId", member.getId());
+
+    return "redirect:/member/profile";
+  }
+
+  @GetMapping("/profile")
+  public String showProfile(HttpSession session, Model model) {
+    Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
+
+    boolean isLogined = loginedMemberId != null;
+
+    if(!isLogined) {
+      return "redirect:/?errorMsg=Need to login!";
+    }
+
+    Member member = memberService.getMemberById(loginedMemberId);
+
+    model.addAttribute("member", member);
+
+    return "member/profile";
   }
 }
--- src/main/java/com/sbs/tutorial/app1/domain/member/service/MemberService.java
+++ src/main/java/com/sbs/tutorial/app1/domain/member/service/MemberService.java
@@ -58,4 +58,8 @@ public class MemberService {
 
     return member;
   }
+
+  public Member getMemberById(Long id) {
+    return memberRepository.findById(id).orElse(null);
+  }
 }
