ad98926644baf682f225ad484e18e90b723bd902
SangWon7242 <gotkdqja@gmail.com>
2025-10-18 11:50:20 +0900

    카카오 로그인 버튼 활성화

--- src/main/java/com/sbs/tutorial/app1/base/config/security/SecurityConfig.java
+++ src/main/java/com/sbs/tutorial/app1/base/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sbs.tutorial.app1.base.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfig {
             .loginProcessingUrl("/member/login") // POST : 로그인 처리
             .defaultSuccessUrl("/")
             .permitAll()
+        ).oauth2Login(Customizer.withDefaults() // 최소한의 설정
         ).logout(logout -> logout
             .logoutUrl("/member/logout") // POST : 로그아웃
             .logoutSuccessUrl("/") // 로그아웃 성공시 리다이렉트
