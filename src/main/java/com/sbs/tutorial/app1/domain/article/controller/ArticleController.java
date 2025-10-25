e7030df6702da3692d4bea2e02ecb68f307c27e3
SangWon7242 <gotkdqja@gmail.com>
2025-10-25 11:16:48 +0900

    게시물 작성시 이미지 파일을 map형태로 받음

--- src/main/java/com/sbs/tutorial/app1/domain/article/controller/ArticleController.java
+++ src/main/java/com/sbs/tutorial/app1/domain/article/controller/ArticleController.java
@@ -1,18 +1,37 @@
 package com.sbs.tutorial.app1.domain.article.controller;
 
+import com.sbs.tutorial.app1.domain.article.input.ArticleForm.ArticleForm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/article")
+@Slf4j
 public class ArticleController {
+
+
   @PreAuthorize("isAuthenticated()")
   @GetMapping("/write")
   public String showWrite() {
     return "article/write";
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @PostMapping("/write")
+  @ResponseBody
+  public String write(ArticleForm articleForm, MultipartRequest multipartRequest) {
+    Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+
+    log.debug("fileMap : {}", fileMap);
+
+    return "작성중";
   }
 }
---
+++ src/main/java/com/sbs/tutorial/app1/domain/article/input/ArticleForm/ArticleForm.java
@@ -0,0 +1,11 @@
+package com.sbs.tutorial.app1.domain.article.input.ArticleForm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArticleForm {
+  private String title;
+  private String content;
+}
