01951e04f56bd33cfc4e65184023ad3f5402b73a
SangWon7242 <gotkdqja@gmail.com>
2025-10-25 10:56:08 +0900

    게시물 작성 폼 구현

---
+++ src/main/java/com/sbs/tutorial/app1/domain/article/controller/ArticleController.java
@@ -0,0 +1,18 @@
+package com.sbs.tutorial.app1.domain.article.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/article")
+public class ArticleController {
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/write")
+  public String showWrite() {
+    return "article/write";
+  }
+}
